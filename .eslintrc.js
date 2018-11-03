06cd135c1c26f46befa9bdd297d099d78c582534
Matthew Miller <kale@iammiller.com>
2018-11-03 00:15:41 -0700

    Allow function() for component attributes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
     "extends": "airbnb-base",
     globals: {
       "AFRAME": true,
+      "document": true,
     },
     rules: {
       // Allow for anonymous functions in things like setTimeout()
       "func-names": "off",
       // Requiring comma-dangle makes for cleaner git diffs
       "comma-dangle": ["error", "always-multiline"],
+      "object-shorthand": "off",
     }
 };
