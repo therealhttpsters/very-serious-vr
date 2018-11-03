3efa0c60336a12ef2ce1860d855a5a3760c42cea
Matthew Miller <kale@iammiller.com>
2018-11-03 00:19:05 -0700

    Add click handlers to pages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 console.log('index');
+
+AFRAME.registerComponent('make-request', {
+  schema: {
+    method: { type: 'string', default: 'GET' },
+    url: { type: 'string' },
+    throttle: { type: 'number', default: 10000 },
+  },
+  init: function () {
+    this.clicked = false;
+
+    this.el.addEventListener('click', () => {
+      if (!this.clicked) {
+        // Prevent further clicks for now
+        this.clicked = true;
+
+        // Do something
+        console.log(this.data.method, this.data.url);
+
+        // Allow clicks after a short timeout
+        setTimeout(() => {
+          console.log('resetting clicked status for', this.el.id);
+          this.clicked = false;
+        }, this.data.throttle);
+      }
+    });
+  },
+});
